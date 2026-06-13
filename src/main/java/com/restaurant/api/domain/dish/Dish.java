dc3a477734c26c22405ca97fa7376a55b141f447
Sweta Ranjan <3.swetaranjan@gmail.com>
2026-06-13 18:33:35 +0530

    Added dish db table

--- src/main/java/com/restaurant/api/domain/dish/Dish.java
+++ src/main/java/com/restaurant/api/domain/dish/Dish.java
@@ -1,4 +1,33 @@
 package com.restaurant.api.domain.dish;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Entity
+@Table(name="dish")
 public class Dish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    @Setter
+    @Column(nullable = false,unique = true)
+    private String name;
+
+    @Setter
+    @Column(name = "price_cents",nullable = false)
+    private Integer priceCents;
+
+    protected Dish(){}
+
+    public Dish(String name,Integer priceCents){
+        this.name = name;
+        this.priceCents = priceCents;
+    }
+
 }
